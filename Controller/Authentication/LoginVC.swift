42ca2f57c73a5f7f3af590e95b24aac358e8a8e8
Rahmatulloh <socialkavinsky@gmail.com>
2022-07-14 15:12:27 +0300

    pdf

--- Controller/Authentication/LoginVC.swift
+++ Controller/Authentication/LoginVC.swift
@@ -5,9 +5,9 @@
 
 //
 import UIKit
-import Firebase
+//import Firebase
 
-class LoginController: UIViewController {
+class LoginVC: UIViewController {
     
     // MARK: - Properties
     
@@ -50,7 +50,7 @@ class LoginController: UIViewController {
         button.heightAnchor.constraint(equalToConstant: 50).isActive = true
         button.layer.cornerRadius = 5
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
-        button.addTarget(self, action: #selector(handleLogin), for: .touchUpInside)
+//        button.addTarget(self, action: #selector(handleLogin), for: .touchUpInside)
         return button
     }()
     
@@ -70,29 +70,29 @@ class LoginController: UIViewController {
     // MARK: - Selectors
     
     @objc func handleShowSignUp() {
-        let controller = RegistrationController()
+        let controller = SignVC()
         navigationController?.pushViewController(controller, animated: true)
     }
     
     @objc func handleLogin() {
         guard let email = emailTextField.text else { return }
         guard let password = passwordTextField.text else { return }
-        
+
         AuthService.shared.logUserIn(withEmail: email, password: password) { (result, error) in
             if let error = error {
                 print("DEBUG: Error logging in \(error.localizedDescription)")
                 return
             }
-            
+
             guard let window = UIApplication.shared.windows.first(where: { $0.isKeyWindow }) else { return }
             guard let tab = window.rootViewController as? MainTabController else { return }
-            
+
             tab.authenticateUserAndConfigureUI()
-            
+
             self.dismiss(animated: true, completion: nil)
         }
     }
-    
+
     // MARK: - Helpers
     
     func configureUI() {
