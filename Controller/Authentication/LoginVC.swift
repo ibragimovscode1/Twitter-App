b2bb08cbb3a12135ad2ec7d932e84ad782610c7e
Rahmatulloh <socialkavinsky@gmail.com>
2022-07-15 15:58:16 +0300

    LOG

--- Controller/Authentication/LoginVC.swift
+++ Controller/Authentication/LoginVC.swift
@@ -75,24 +75,27 @@ class LoginVC: UIViewController {
         self.navigationController?.pushViewController(controller, animated: true)
     }
     
-//    @objc func handleLogin() {
-//        guard let email = emailTextField.text else { return }
-//        guard let password = passwordTextField.text else { return }
+    @objc func handleLogin() {
+        guard let email = emailTextField.text else { return }
+        guard let password = passwordTextField.text else { return }
+
+        AuthService.shared.logUserIn(withEmail: email, password: password) { (result, error) in
+            if let error = error {
+                print("DEBUG: Error logging in \(error.localizedDescription)")
+                return
+            }
+            print("succesful log in...")
+        }
+    }
 //
-////        AuthService.shared.logUserIn(withEmail: email, password: password) { (result, error) in
-////            if let error = error {
-////                print("DEBUG: Error logging in \(error.localizedDescription)")
-////                return
-////            }
-////
-////            guard let window = UIApplication.shared.windows.first(where: { $0.isKeyWindow }) else { return }
-////            guard let tab = window.rootViewController as? MainTabController else { return }
-////
-////            tab.authenticateUserAndConfigureUI()
-////
-////            self.dismiss(animated: true, completion: nil)
-////        }
-//    }
+//            guard let window = UIApplication.shared.windows.first(where: { $0.isKeyWindow }) else { return }
+//            guard let tab = window.rootViewController as? MainTabController else { return }
+//
+//            tab.authenticateUserAndConfigureUI()
+//
+//            self.dismiss(animated: true, completion: nil)
+//        }
+    
 
     // MARK: - Helpers
     
--- Controller/MainTabBarController.swift
+++ Controller/MainTabBarController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class MainTabBarController: UITabBarController {
     //MARK: - Properties
@@ -22,13 +23,39 @@ class MainTabBarController: UITabBarController {
     //MARK: - LifeCycle
         override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
+        view.backgroundColor = .white
         self.tabBar.isTranslucent = false
-        configure()
-        configureUI()
+//            logUserOut()
+        authenticateUserAndConfigure()
+      
       
         
     }
+    //MARK: -  API
+    func authenticateUserAndConfigure() {
+        
+        if Auth.auth().currentUser == nil {
+            DispatchQueue.main.async {
+                let nav = UINavigationController(rootViewController: LoginVC())
+                nav.modalPresentationStyle = .fullScreen
+                self.present(nav, animated: true, completion: nil)
+            }
+            print("Debug: user is not logged in")
+        } else {
+            configureVC()
+            configureUI()
+        }
+        
+    }
+    func logUserOut() {
+        do {
+            try Auth.auth().signOut()
+            print("loged out")
+        } catch let error {
+            print("DEBUG: Failed to sign out with error  \(error.localizedDescription)")
+        }
+    }
+   
     //MARK: - Selectors
     
     @objc func actionButtonTapped() {
@@ -42,7 +69,7 @@ class MainTabBarController: UITabBarController {
         actionButton.layer.cornerRadius = 56/2
     }
     
-    func configure() {
+    func configureVC() {
         let home = HomeVC()
         let nav1 = templateNavController(image: UIImage(systemName: "house"), rootViewController: home)
         
